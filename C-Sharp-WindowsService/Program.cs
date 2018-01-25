6a33872fc1ea4a218fd03bb0ba06da92f354a1d9
ITCSs Developer <itcss.developer.me@gmail.com>
2018-01-25 11:52:43 +0700

    Initialize Project

---
+++ C-Sharp-WindowsService/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace C_Sharp_WindowsService
+{
+    internal static class Program
+    {
+        /// REF : http://www.thaicreate.com/tutorial/windows-service-application.html
+        private static void Main()
+        {
+#if DEBUG
+            var myService = new Service1();
+            myService.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+        ServiceBase[] ServicesToRun;
+        ServicesToRun = new ServiceBase[]
+        {
+            new Service1()
+        };
+        ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ C-Sharp-WindowsService/Service1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace C_Sharp_WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            this.OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var strPath = AppDomain.CurrentDomain.BaseDirectory + "Log.txt";
+            System.IO.File.AppendAllLines(strPath, new[] { "Starting time : " + DateTime.Now.ToString() });
+            this.timer1.Start();
+        }
+
+        protected override void OnStop()
+        {
+            var strPath = AppDomain.CurrentDomain.BaseDirectory + "Log.txt";
+            System.IO.File.AppendAllLines(strPath, new[] { "Stop time : " + DateTime.Now.ToString() });
+            this.timer1.Stop();
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string strPath = AppDomain.CurrentDomain.BaseDirectory + "Log.txt";
+            System.IO.File.AppendAllLines(strPath, new[] { "..calling time : " + DateTime.Now.ToString() });
+        }
+    }
+}
